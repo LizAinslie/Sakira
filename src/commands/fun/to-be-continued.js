6f52485dbfff00531ee8184c0e4252c0cf3f0ee7
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 20:49:11 -0500

    try?

--- src/commands/fun/to-be-continued.js
+++ src/commands/fun/to-be-continued.js
@@ -20,7 +20,7 @@ module.exports = class ToBeContinuedCommand extends Command {
 				{
 					key: 'image',
 					prompt: 'What image would you like to edit?',
-					type: 'image',
+					type: 'image|avatar',
 					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
 				}
 			]
