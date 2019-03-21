a53594cc5cd60009bf38b11a43d59c81be80754c
Axel Greavette <vultureavatar@gmail.com>
2019-03-21 17:29:52 -0400

    s

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -6,6 +6,7 @@ module.exports = class SakiraClient extends Commando.CommandoClient {
     constructor(options) {
         super(options)
 
+        this.webhook = {}
         this.webhook.KSC = new WebhookClient(KSC_WH_ID, KSC_WH_TOKEN, { disableEveryone: true })
         this.webhook.PMD = new WebhookClient(PMD_WH_ID, PMD_WH_TOKEN, { disableEveryone: true })
     }
